541625916e7e7020dcb172337a5c4126d495dcb5
azharali8907 <60216443+azharali8907@users.noreply.github.com>
2020-06-20 05:02:30 -0700

    update App.css

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Transactions = ({ transaction }) => {
     return(
-    <li className="postive">
+    <li className={transaction.amount < 0 ? 'nagtive':'postive'}>
                     {transaction.discrption}
                     <span>{transaction.amount}</span>
                     <button className="delbtn">x</button>
